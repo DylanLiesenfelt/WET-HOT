b776a5707f9041d0329762c30df7aff0412e1cff
DylanLiesenfelt <dylanjames.code@gmail.com>
2024-09-17 00:08:14 -0400

    build: toolbar icon

--- build/main.js
+++ build/main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path'); // You need this for the path.join
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -6,10 +7,13 @@ function createWindow() {
         height: 600,
         webPreferences: {
             nodeIntegration: true,
-    },
-});
+        },
+        icon: path.join(__dirname, '../assets/images/icons/wet-hot-icon.png'), // __dirname ensures correct path
+    });
 
-win.loadFile('../index.html');
+    //win.removeMenu(); // Uncomment on release to remove the menu
+
+    win.loadFile(path.join(__dirname, '../index.html')); // __dirname ensures relative path from the current file
 }
 
 app.whenReady().then(createWindow);
