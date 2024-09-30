0a76a8b9738e7f7adc673fb2a1939b5dc7affb17
DylanLiesenfelt <dylanjames.code@gmail.com>
2024-09-30 00:53:00 -0400

    Build: Start Menu Rebuild

--- assets/src/newGame.js
+++
@@ -1,6 +0,0 @@
-const NEW_GAME_BTN = document.getElementById('new-game-btn');
-
-NEW_GAME_BTN.addEventListener('click', function() {
-    window.location.href = "./assets/pages/characterCreation.html";
-});
-
---
+++ assets/src/startMenu.js
@@ -0,0 +1,6 @@
+const START = document.getElementById('start-btn');
+const LOAD = document.getElementById('load-btn');
+
+START.addEventListener('click', function(){
+    window.location.href = ""
+})
